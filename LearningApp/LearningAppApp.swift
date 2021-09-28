fdaa91db6356433df90f828fc5bc43225e420f3f
Muhammadjon <20mamarasulov@gmail.com>
2021-09-28 09:35:12 +0900

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Muhammadjon Mamarasulov on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
